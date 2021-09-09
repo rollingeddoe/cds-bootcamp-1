5166d516c8ac043ad0321dcc08a92cc31fae1ffc
Wenda Zhou <wz2247@nyu.edu>
2021-09-08 23:32:31 -0400

    fix issue in singularity script

--- scripts/run_instance_singularity.sh
+++ scripts/run_instance_singularity.sh
@@ -1,21 +1,19 @@
 #! /bin/bash
 
 
-ENV_PREFIX=/ext3/conda/pytorch
-
-
 set -e
 set -u
 
+SSH_KEY_DIR=/ext3/ssh
+mkdir -p $SSH_KEY_DIR
+
 if [[ ! -f $HOME/.ssh/ssh_host_rsa_key ]]; then
-ssh-keygen -f $HOME/.ssh/ssh_host_rsa_key -N '' -t rsa -b 2048
+ssh-keygen -f $SSH_KEY_DIR/ssh_host_rsa_key -N '' -t rsa -b 2048
 fi
 
 if [[ ! -f $HOME/.ssh/ssh_host_ecdsa_key ]]; then
-ssh-keygen -f $HOME/.ssh/ssh_host_ecdsa_key -N '' -t ecdsa -b 256
+ssh-keygen -f $SSH_KEY_DIR/ssh_host_ecdsa_key -N '' -t ecdsa -b 256
 fi
-
-conda activate $ENV_PREFIX
 
 # This is the port on the GCP instance that will be used by the SSH daemon
 SSHD_PORT=16453
@@ -24,6 +22,6 @@ SSHD_PATH=$(which sshd)
 # Start new ssh daemon binding to user port. We specify the config file as /dev/null
 # to prevent sshd from reading the default config file (which requires root)
 echo "Starting SSH daemon"
-$SSHD_PATH -f /dev/null -p $SSHD_PORT -h ~/.ssh/ssh_host_rsa_key -h ~/.ssh/ssh_host_ecdsa_key
+$SSHD_PATH -f /dev/null -p $SSHD_PORT -h $SSH_KEY_DIR/ssh_host_rsa_key -h $SSH_KEY_DIR/ssh_host_ecdsa_key
 
 sleep infinity
